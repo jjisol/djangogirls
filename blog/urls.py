cd3d6aca4fc5d4fd0cc72265a9a8345ce1361f10
prerana <preranakorea@gmail.com>
2019-10-27 21:02:27 +0900

    changed the HTML for the site

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('main/', views.post_list, name='post_list'),
+    path('signup/', views.signup, name='signup'),
+    path('class_details/', views.class_details, name='class_details'),
+    path('instructor_details/', views.instructor_details, name='instructor_details'),
+    path('hotclass_details/', views.hotclass_details, name='hotclass_details'),
+    path('yoga_details/', views.yoga_details, name='yoga_details'),
+    path('oneday_class_details/', views.oneday_class_details, name='oneday_class_details'),
+    path('pilates_details/', views.pilates_details, name='pilates_details'),
+    path('view_on_map/', views.view_on_map, name='view_on_map'),
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,31 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
-# Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/carousel.html', {})
+
+def signup(request):
+    return render(request, 'blog/signup.html', {})
+
+def class_details(request):
+    return render(request, 'blog/class_details.html', {})
+
+def instructor_details(request):
+    return render(request, 'blog/instructor_details.html', {})
+
+def hotclass_details(request):
+    return render(request, 'blog/hotclass_details.html', {})
+
+def yoga_details(request):
+    return render(request, 'blog/yoga_details.html', {})
+
+def oneday_class_details(request):
+    return render(request, 'blog/oneday_class_details.html', {})
+
+def pilates_details(request):
+    return render(request, 'blog/pilates_details.html', {})
+
+def view_on_map(request):
+    return render(request, 'blog/view_on_map.html', {})
